16aa0241b81ef619ae824212a3d527d7c5442201
sergio alvarez pinto <sertech_live@hotmail.com>
2018-07-14 00:19:46 -0400

    new port 8000

--- project3.py
+++ project3.py
@@ -226,5 +226,5 @@ def allCatalogJSON():
 if __name__ == '__main__':
     app.secret_key = 'do not share this baby'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
     
